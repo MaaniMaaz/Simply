b1509ee0422e6d7f7a22b9fa5153fe9dc62d5bfa
MaaniMaaz <124836374+MaaniMaaz@users.noreply.github.com>
2025-01-19 16:54:24 +0500

    check now
    check now

--- src/api/config.js
+++ src/api/config.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' 
-        ? 'http://localhost:5000/api' 
-        : 'https://simply-backend-yl91.onrender.com/api',
+    baseURL: 'https://simply-backend-yl91.onrender.com/api',
     headers: {
-        'Content-Type': 'application/json',
-    },
+        'Content-Type': 'application/json'
+    }
 });
-
 
 // Add a request interceptor to add auth token to requests
 API.interceptors.request.use(
